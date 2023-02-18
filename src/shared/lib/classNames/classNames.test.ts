96de0996fac243824ed5e37222ba16959ecccb1a
ferushen <fbaka@list.ru>
2023-02-18 09:46:34 +0300

    add jest, add first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,47 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+	test('with only one param', () => {
+		expect(classNames('someClass')).toBe('someClass');
+	});
+
+	test('with additional class', () => {
+		const expected = 'someClass addClassOne addClassTwo';
+		expect(classNames('someClass', {}, ['addClassOne', 'addClassTwo'])).toBe(
+			expected
+		);
+	});
+
+	test('with mods', () => {
+		const expected = 'someClass modeClassOne modeClassTree';
+		expect(
+			classNames(
+				'someClass',
+				{ modeClassOne: true, modeClassTwo: false, modeClassTree: true },
+				[]
+			)
+		).toBe(expected);
+	});
+
+	test('with additional classes and mods', () => {
+		const expected =
+			'someClass addClassOne addClassTwo modeClassOne modeClassTwo';
+		expect(
+			classNames(
+				'someClass',
+				{ modeClassOne: true, modeClassTwo: true, modeClassTree: false },
+				['addClassOne', 'addClassTwo']
+			)
+		).toBe(expected);
+	});
+
+	test('with only additional classes', () => {
+		const expected = 'addClassOne addClassTwo';
+		expect(classNames('', {}, ['addClassOne', 'addClassTwo'])).toBe(expected);
+	});
+
+	test('empty params', () => {
+		const expected = '';
+		expect(classNames('')).toBe(expected);
+	});
+});
