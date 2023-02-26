00dce308474da67069b980c1472fa26e9b0ba496
ferushen <fbaka@list.ru>
2023-02-26 17:24:25 +0300

    fix preview.js import

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -2,7 +2,7 @@ import { addDecorator } from '@storybook/react';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator';
-import { Theme } from '../../src/app/providers/themeProvider/lib/themeContext';
+import { Theme } from '../../src/app/providers/themeProvider';
 
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
