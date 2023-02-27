6cea011fff580bf67a586b93d1e22c07a0c2c712
ferushen <fbaka@list.ru>
2023-02-27 18:19:25 +0300

    delete export default

--- src/app/providers/ThemeProvider/index.ts
+++ src/app/providers/ThemeProvider/index.ts
@@ -1,3 +1,3 @@
-export { default as ThemeProvider } from './ui/themeProvider';
+export { ThemeProvider } from './ui/themeProvider';
 export { useTheme } from './lib/useTheme';
 export { Theme } from './lib/themeContext';
--- src/app/providers/ThemeProvider/ui/themeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/themeProvider.tsx
@@ -3,7 +3,7 @@ import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/themeContex
 
 const defaultValue = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
-const ThemeProvider: FC = ({ children }) => {
+export const ThemeProvider: FC = ({ children }) => {
 	const [theme, setTheme] = useState<Theme>(defaultValue);
 
 	const defaultProps = useMemo(() => ({
@@ -17,5 +17,3 @@ const ThemeProvider: FC = ({ children }) => {
 		</ThemeContext.Provider>
 	);
 };
-
-export default ThemeProvider;
